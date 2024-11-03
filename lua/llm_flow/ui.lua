62095e3ac19ee335bd6f719a6a82ebf0604eb692
Dima Lipin <dimich3d@ya.ru>
2024-11-03 13:08:52 +0100

    feat: add example function to set virtual text

--- lua/llm_flow/ui.lua
+++ lua/llm_flow/ui.lua
@@ -1,6 +1,3 @@
-
-
-
 local M = {}
 
 -- Creates virtual (ghost) text at the specified position
@@ -10,16 +7,20 @@ local M = {}
 function M.set_text(line, pos, text)
   local bufnr = vim.api.nvim_get_current_buf()
   local ns_id = vim.api.nvim_create_namespace('llm_flow')
-  
+
   -- Clear any existing virtual text in this namespace
   vim.api.nvim_buf_clear_namespace(bufnr, ns_id, 0, -1)
-  
+
   -- Create virtual text
   vim.api.nvim_buf_set_extmark(bufnr, ns_id, line, pos, {
-    virt_text = {{text, 'Comment'}},
+    virt_text = { { text, 'Comment' } },
     virt_text_pos = 'inline',
     hl_mode = 'combine',
   })
+end
+
+function M.example()
+  M.set_text(0, 0, "benis")
 end
 
 return M
